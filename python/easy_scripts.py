0d5d1c9fe232d21e34bffa28c57864387986c617
i.kozlov <kozlov.titan2010@yandex.ru>
2021-04-23 11:22:14 +0300

    WorkFlow. Ivan Kozlov. Scripts

--- python/easy_scripts.py
+++ python/easy_scripts.py
@@ -3,7 +3,7 @@
 Задание пути для файла относительно корня проекта.
 
 Запуск питона питоном. НЕ ПЕРЕМЕЩАТЬ, чтобы не сломать path_general_folder
-Hello World
+Hello World. 2
 """
 
 from pathlib import Path
